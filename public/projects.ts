40155b2fd0098576bc74d78de94d0cdd016b188d
marthaloh <104151769+marthaloh@users.noreply.github.com>
2022-09-06 09:39:08 +0200

    Add typetodelete

--- public/projects.ts
+++ public/projects.ts
@@ -10,14 +10,15 @@ interface Project {
 }
 export const projects: Project[] = [
 	{
-		title: "",
-		description: "",
+		title: "typetodelete",
+		description: "typetodelete is a useless text editor who fills the page with his own text. To use it you first have to clear the page by typing the editor's text faster than him adding new words.",
 		authors: [
 			{
-				name: "",
-				github: "",
+				name: "marthaloh",
+				github: "https://github.com/marthaloh",
 			},
 		],
-		source: "",
+		source: "https://github.com/hawk-de/nutzloses-netz-marthaloh",
+		url: "https://typetodelete.com",
 	},
 ];
