5cc3671c3dc594f15ca178fa4a077341e5eab4e5
ff6347 <hash@ff6347.email>
2023-06-09 21:48:30 +0200

    Add a new project to the list of projects.

--- public/projects.ts
+++ public/projects.ts
@@ -7,11 +7,13 @@ interface Project {
 	description: string;
 	authors: Author[];
 	source: string;
+	url: string;
 }
 export const projects: Project[] = [
 	{
 		title: "typetodelete",
-		description: "typetodelete is a useless text editor who fills the page with his own text. To use it you first have to clear the page by typing the editor's text faster than him adding new words.",
+		description:
+			"typetodelete is a useless text editor who fills the page with his own text. To use it you first have to clear the page by typing the editor's text faster than him adding new words.",
 		authors: [
 			{
 				name: "marthaloh",
@@ -22,15 +24,65 @@ export const projects: Project[] = [
 		url: "https://typetodelete.com",
 	},
 	{
-    "title": "Frogchestra",
-    "description": "Frogchestra is an online desktop application where all the keys in your piano are frogs, and you can play them with your keyboard. Play your favourite songs, compose thoughtful moonlight sonatas, or just hit random keys to create a unique atmosphere.",
-    "authors": [
-        {
-            "name": "Simon Keller",
-            "github": "https://github.com/simonster12"
-        }
-    ],
-    "source": "https://github.com/hawk-de/nutzloses-netz-simonster12",
-    "url": "https://frogchestra.site/"
-}
+		title: "YOU WASTE YOUR TIME",
+		description:
+			"This is a useless website full of time eating stuff to explore",
+		authors: [
+			{
+				name: "Mia Vogl",
+				github: "miamogl",
+			},
+			{
+				name: "Mia Vogl",
+				github: "Cherise Barthel",
+			},
+		],
+		source: "https://github.com/hawk-de/nutzloses-netz-cherise-mia",
+		url: "https://youwasteyourti.me/",
+	},
+	{
+		title: "Frogchestra",
+		description:
+			"Frogchestra is an online desktop application where all the keys in your piano are frogs, and you can play them with your keyboard. Play your favourite songs, compose thoughtful moonlight sonatas, or just hit random keys to create a unique atmosphere.",
+		authors: [
+			{
+				name: "Simon Keller",
+				github: "https://github.com/simonster12",
+			},
+		],
+		source: "https://github.com/hawk-de/nutzloses-netz-simonster12",
+		url: "https://frogchestra.site/",
+	},
+	{
+		authors: [{ github: "Liarikai", name: "Natallia" }],
+		url: "https://www.beamenace.lol/",
+		source: "https://github.com/hawk-de/nutzloses-netz-Liarikai",
+		title: "Be a Menace",
+		description: `A mildly entertaining website that you'll visit once, maybe twice, and then never return to it again, because why would you. It's a useless website after all, and you probably have some more important stuff to do than annoying a blue hand-animated troll.`,
+	},
+	{
+		title: "Tricky Target",
+		description: "Shoot the target.",
+		authors: [
+			{
+				name: "JoshuaNiemann",
+				github: "Joshua Niemann",
+			},
+		],
+		url: "https://trickytarget.art/",
+		source: "https://github.com/hawk-de/nutzloses-netz-JoshuaNiemann",
+	},
+	{
+		authors: [
+			{
+				name: "Hauke Rosendahl",
+				github: "HaukeRosendahl",
+			},
+		],
+		description:
+			"Trash-Collector is a game where you control a spaceship. Your mission is to get rid of all the trash in space. You do that by shooting the trash with a laser.",
+		url: "https://trash-collector.art/",
+		source: "https://github.com/hawk-de/nutzloses-netz-HaukeRosendahl",
+		title: "Trash Collector",
+	},
 ];
